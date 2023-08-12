e192f0e1bf4f24a35a9bed7fc16af44241aa3cdf
Jabs <8946581+Jabser@users.noreply.github.com>
2023-08-12 15:10:02 +0200

    Update weather.js
    Extra data points added

--- src/components/weather.js
+++ src/components/weather.js
@@ -7,6 +7,10 @@ const WeatherCard = ({weatherData}) => {
       <Card centered fluid className="weather-card">
         <Card.Content>
           <Card.Header className="header">{weatherData.name}</Card.Header>
+          <p>Temperature: {weatherData.main.temp}</p>
+          <p>Sunrise: {weatherData.sys.sunrise}</p>
+          <p>Sunset: {weatherData.sys.sunset}</p>
+          <p>Description: {weatherData.weather[0].description}</p>
         </Card.Content>
       </Card>
     </Container>
