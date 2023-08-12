431d8e281b6cf073f9eb76f204b8834a2c3325e3
Jabs <8946581+Jabser@users.noreply.github.com>
2023-08-12 17:34:10 +0200

    Update weather.js

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Card, Container } from 'semantic-ui-react';
+import moment from "moment/moment";
 
 const WeatherCard = ({weatherData}) => {
   return (
     <Container>
       <Card centered fluid className="weather-card">
         <Card.Content>
-          <Card.Header className="header">{weatherData.name}</Card.Header>
-          <p>Temperature: {weatherData.main.temp}</p>
-          <p>Sunrise: {weatherData.sys.sunrise}</p>
-          <p>Sunset: {weatherData.sys.sunset}</p>
-          <p>Description: {weatherData.weather[0].description}</p>
+          <Card.Header className="header">City name: {weatherData.name}</Card.Header>
+          <p>Temperature: {weatherData.main.temp} &deg;C</p>
+          <p>Sunrise: {weatherData.sys.sunrise * 1000}</p>
+          <p>Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('da-DK')}</p>
+          <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('da-DK')}</p>
+          <p>Description: {weatherData.weather[0].main}</p>
+          <p>Humidity: {weatherData.main.humidity} %</p>
+
+          <p>Day: {moment().format('dddd')}</p>
+          <p>Date: {moment().format('LL')}</p>
         </Card.Content>
       </Card>
     </Container>
