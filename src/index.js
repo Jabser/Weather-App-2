7a16b3fdb91d43b06429c60cca50e043dd7781df
Jabs <8946581+Jabser@users.noreply.github.com>
2023-08-14 13:45:03 +0200

    SASS support

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
 
-import './App.css';
+import './App.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
